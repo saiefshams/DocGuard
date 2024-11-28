1ba8f0ff6b7ccba24b65defb5d62f203b20d19c6
calebfontaine <caleb.fontaine@ontariotechu.net>
2024-11-27 23:47:14 -0500

    update code

--- controller/control.py
+++ controller/control.py
@@ -34,65 +34,6 @@ is_verified = verify_document_hash('example_document.txt')  # Replace with your 
 print(f"Document verified: {is_verified}")
 
 
-#########################
-#Interface of application#
-
-from flask import Flask, request, render_template
-import hashlib
-
-app = Flask(__name__)
-document_hashes = {}
-
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-@app.route('/upload', methods=['POST'])
-def upload_document():
-    document = request.files['document']
-    document_content = document.read()
-    document_hash = hashlib.sha256(document_content).hexdigest()
-    document_hashes[document_hash] = True
-    return 'Document uploaded and hash stored.'
-
-@app.route('/verify', methods=['POST'])
-def verify_document():
-    document = request.files['document']
-    document_content = document.read()
-    document_hash = hashlib.sha256(document_content).hexdigest()
-    if document_hashes.get(document_hash):
-        return 'Document is verified.'
-    else:
-        return 'Document verification failed.'
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
-    
-###############################
-#Interface of application#
-
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Document Verification System</title>
-</head>
-<body>
-    <h1>Upload Document</h1>
-    <form action="/upload" method="post" enctype="multipart/form-data">
-        <input type="file" name="document">
-        <input type="submit" value="Upload">
-    </form>
-
-    <h1>Verify Document</h1>
-    <form action="/verify" method="post" enctype="multipart/form-data">
-        <input type="file" name="document">
-        <input type="submit" value="Verify">
-    </form>
-</body>
-</html>
-
-
 #####################
 #Implementation of Blockchain Technology#
 
